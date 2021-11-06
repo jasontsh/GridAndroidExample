a149189cace457f746ef5e22acd63df4d626c0f7
Jason <tangsh@sas.upenn.edu>
2021-11-06 01:15:18 -0700

    Update splash screen solution to version 12

--- app/src/main/java/com/jasontsh/interviewkickstart/gridlayoutexample/ItemFragment.kt
+++ app/src/main/java/com/jasontsh/interviewkickstart/gridlayoutexample/ItemFragment.kt
@@ -14,11 +14,6 @@ import com.jasontsh.interviewkickstart.gridlayoutexample.placeholder.Placeholder
  * A fragment representing a list of Items.
  */
 class ItemFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,20 +34,5 @@ class ItemFragment : Fragment() {
             }
         }
         return view
-    }
-
-    companion object {
-
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        // TODO: Customize parameter initialization
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-            ItemFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
-                }
-            }
     }
 }
--- app/src/main/java/com/jasontsh/interviewkickstart/gridlayoutexample/MainActivity.kt
+++ app/src/main/java/com/jasontsh/interviewkickstart/gridlayoutexample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jasontsh.interviewkickstart.gridlayoutexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.commit
@@ -9,6 +10,7 @@ import androidx.fragment.app.commit
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.frame_layout)
         val manager:FragmentManager = supportFragmentManager
         manager.commit {
